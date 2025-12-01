6a35e265efd67a80666f38d0f029a1dcb599d94c
Zhuoyu Lin <linzhuoyu@aitopia.tech>
2025-12-01 14:50:05 +0800

    fix

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.s6innr1slus"
+    "revision": "0.833n3ctcsv8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
